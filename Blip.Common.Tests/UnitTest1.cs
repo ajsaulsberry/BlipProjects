76063d5f61b2799dfc55c953ccced3eb1dc303a7
ajsaulsberry <ajsaulsberry@outlook.com>
2017-08-22 13:05:08 -0400

    Added Blip.Common .NET Framework blank project with cocreation of Blip.Common.Test unit test project.

---
+++ Blip.Common.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blip.Common.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
