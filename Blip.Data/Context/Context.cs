7ae6f7e6da34b7c9d0da01a2afaed71047ef96b4
ajsaulsberry <ajsaulsberry@outlook.com>
2017-08-22 18:26:29 -0400

    Added Context.cs. Added reference to Blip.Entities entity namespaces.

---
+++ Blip.Data/Context/Context.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Blip.Entities.Customer;
+using Blip.Entities.Geographies;
+
+namespace BlipDrop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("ApplicationDbContext")
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
